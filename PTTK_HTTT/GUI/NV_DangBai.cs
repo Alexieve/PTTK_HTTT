f36d5f3b665a0c02c00ce9e657ddc138e268607a
Thinh Vuong <94957063+thinhcu67si@users.noreply.github.com>
2024-04-07 00:27:51 +0700

    ui/7/4
    hihi

---
+++ PTTK_HTTT/GUI/NV_DangBai.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+
+    public partial class NV_DangBai : Form
+    {
+        private Button currentButton;
+        private Form activeForm = null;
+        public NV_DangBai()
+        {
+            InitializeComponent();
+        }
+        private void openChildForm(Form childForm)
+        {
+            if (activeForm != null)
+            {
+                activeForm.Close();
+            }
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            this.childFormPanel.Controls.Add(childForm);
+            this.childFormPanel.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+        private void ActiveButton(object senderBtn)
+        {
+            if (senderBtn != null)
+            {
+                if (currentButton != (Button)senderBtn)
+                {
+                    DisableButton();
+                    currentButton = (Button)senderBtn;
+                    currentButton.BackColor = Color.FromArgb(128, 128, 128);
+                }
+            }
+        }
+        private void DisableButton()
+        {
+            if (currentButton != null)
+            {
+                currentButton.BackColor = Color.FromArgb(64, 64, 64);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openChildForm(new NV_TraCuuHD());
+            ActiveButton(sender);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            openChildForm(new NV_TraCuuHD());
+            ActiveButton(sender);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openChildForm(new NV_TraCuuHD());
+            ActiveButton(sender);
+        }
+    }
+}
