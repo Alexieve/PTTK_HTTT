4cad9878b0ef7cb6396a99fa39c87376ce73d1de
Thinh Vuong <94957063+thinhcu67si@users.noreply.github.com>
2024-05-06 22:19:22 +0700

    Update NV_TraCuuHD_DangBai.Designer.cs

--- PTTK_HTTT/GUI/NV/NV_TraCuuHD_DangBai.Designer.cs
+++ PTTK_HTTT/GUI/NV/NV_TraCuuHD_DangBai.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace GUI
+namespace GUI
 {
-    partial class NV_TraCuuHD_DangBai
+    partial class NV_TraCuuHD_XemBaiDang
     {
         /// <summary>
         /// Required designer variable.
@@ -36,25 +36,28 @@
             textBox1 = new TextBox();
             textBox5 = new TextBox();
             label6 = new Label();
+            textBox2 = new TextBox();
             label4 = new Label();
             textBox3 = new TextBox();
             label5 = new Label();
+            textBox4 = new TextBox();
             label7 = new Label();
+            textBox6 = new TextBox();
             label1 = new Label();
+            textBox7 = new TextBox();
             label8 = new Label();
             textBox8 = new TextBox();
             label9 = new Label();
             textBox9 = new TextBox();
             label10 = new Label();
             childFormPanel = new Panel();
-            dateTimePicker1 = new DateTimePicker();
+            button5 = new Button();
+            button4 = new Button();
             button3 = new Button();
-            cbboxCapbac = new ComboBox();
-            nbSoluong = new NumericUpDown();
-            dtpkNgayTD = new DateTimePicker();
+            label11 = new Label();
+            textBox10 = new TextBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             childFormPanel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)nbSoluong).BeginInit();
             SuspendLayout();
             // 
             // label3
@@ -65,12 +68,12 @@
             label3.Name = "label3";
             label3.Size = new Size(1075, 52);
             label3.TabIndex = 5;
-            label3.Text = "Đăng bài";
+            label3.Text = "Chi tiết bài đăng";
             label3.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // button2
             // 
-            button2.BackColor = Color.Plum;
+            button2.BackColor = Color.Turquoise;
             button2.Location = new Point(41, 60);
             button2.Name = "button2";
             button2.Size = new Size(198, 40);
@@ -81,19 +84,21 @@
             // 
             // button1
             // 
-            button1.Location = new Point(470, 658);
+            button1.Location = new Point(783, 648);
             button1.Name = "button1";
-            button1.Size = new Size(128, 40);
+            button1.Size = new Size(148, 40);
             button1.TabIndex = 11;
-            button1.Text = "Đăng";
+            button1.Text = "Hiệu chỉnh";
             button1.Click += button1_Click;
             // 
             // pictureBox1
             // 
             pictureBox1.BackColor = Color.RosyBrown;
+            pictureBox1.Image = Properties.Resources.tải_xuống;
             pictureBox1.Location = new Point(41, 120);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(245, 218);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 12;
             pictureBox1.TabStop = false;
             // 
@@ -101,7 +106,7 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
-            label2.Location = new Point(299, 118);
+            label2.Location = new Point(320, 122);
             label2.Name = "label2";
             label2.Size = new Size(190, 28);
             label2.TabIndex = 13;
@@ -111,33 +116,43 @@
             // 
             textBox1.BackColor = SystemColors.Window;
             textBox1.Font = new Font("Segoe UI", 12F);
-            textBox1.Location = new Point(509, 115);
+            textBox1.Location = new Point(530, 119);
             textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
             textBox1.Size = new Size(217, 34);
             textBox1.TabIndex = 14;
             // 
             // textBox5
             // 
             textBox5.BackColor = SystemColors.Window;
-            textBox5.Font = new Font("Segoe UI", 10F);
-            textBox5.Location = new Point(509, 217);
+            textBox5.Location = new Point(530, 210);
             textBox5.Name = "textBox5";
-            textBox5.Size = new Size(217, 30);
+            textBox5.ReadOnly = true;
+            textBox5.Size = new Size(217, 27);
             textBox5.TabIndex = 18;
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(299, 220);
+            label6.Location = new Point(320, 213);
             label6.Name = "label6";
             label6.Size = new Size(135, 20);
             label6.TabIndex = 17;
             label6.Text = "Tên doanh nghiệp: ";
             // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.Window;
+            textBox2.Location = new Point(438, 290);
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.Size = new Size(229, 27);
+            textBox2.TabIndex = 20;
+            // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(299, 308);
+            label4.Location = new Point(320, 290);
             label4.Name = "label4";
             label4.Size = new Size(70, 20);
             label4.TabIndex = 19;
@@ -146,42 +161,70 @@
             // textBox3
             // 
             textBox3.BackColor = SystemColors.Window;
-            textBox3.Location = new Point(133, 419);
+            textBox3.Location = new Point(133, 391);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(301, 27);
+            textBox3.ReadOnly = true;
+            textBox3.Size = new Size(861, 27);
             textBox3.TabIndex = 22;
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(41, 422);
+            label5.Location = new Point(41, 394);
             label5.Name = "label5";
             label5.Size = new Size(62, 20);
             label5.TabIndex = 21;
             label5.Text = "Địa chỉ: ";
             // 
+            // textBox4
+            // 
+            textBox4.BackColor = SystemColors.Window;
+            textBox4.Location = new Point(530, 250);
+            textBox4.Name = "textBox4";
+            textBox4.ReadOnly = true;
+            textBox4.Size = new Size(151, 27);
+            textBox4.TabIndex = 24;
+            // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(299, 264);
+            label7.Location = new Point(320, 250);
             label7.Name = "label7";
             label7.Size = new Size(154, 20);
             label7.TabIndex = 23;
             label7.Text = "Số lượng tuyển dụng: ";
             // 
+            // textBox6
+            // 
+            textBox6.BackColor = SystemColors.Window;
+            textBox6.Location = new Point(530, 170);
+            textBox6.Name = "textBox6";
+            textBox6.ReadOnly = true;
+            textBox6.Size = new Size(151, 27);
+            textBox6.TabIndex = 26;
+            // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(299, 169);
+            label1.Location = new Point(320, 169);
             label1.Name = "label1";
             label1.Size = new Size(89, 20);
             label1.TabIndex = 25;
             label1.Text = "Ngày đăng: ";
             // 
+            // textBox7
+            // 
+            textBox7.BackColor = SystemColors.Window;
+            textBox7.Location = new Point(892, 166);
+            textBox7.Name = "textBox7";
+            textBox7.ReadOnly = true;
+            textBox7.Size = new Size(151, 27);
+            textBox7.TabIndex = 28;
+            // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(741, 169);
+            label8.Location = new Point(748, 169);
             label8.Name = "label8";
             label8.Size = new Size(107, 20);
             label8.TabIndex = 27;
@@ -190,9 +233,10 @@
             // textBox8
             // 
             textBox8.BackColor = SystemColors.Window;
-            textBox8.Location = new Point(133, 462);
+            textBox8.Location = new Point(133, 434);
             textBox8.Multiline = true;
             textBox8.Name = "textBox8";
+            textBox8.ReadOnly = true;
             textBox8.ScrollBars = ScrollBars.Vertical;
             textBox8.Size = new Size(861, 100);
             textBox8.TabIndex = 30;
@@ -200,7 +244,7 @@
             // label9
             // 
             label9.AutoSize = true;
-            label9.Location = new Point(41, 462);
+            label9.Location = new Point(41, 434);
             label9.Name = "label9";
             label9.Size = new Size(55, 20);
             label9.TabIndex = 29;
@@ -209,15 +253,16 @@
             // textBox9
             // 
             textBox9.BackColor = SystemColors.Window;
-            textBox9.Location = new Point(133, 577);
+            textBox9.Location = new Point(133, 549);
             textBox9.Name = "textBox9";
+            textBox9.ReadOnly = true;
             textBox9.Size = new Size(301, 27);
             textBox9.TabIndex = 32;
             // 
             // label10
             // 
             label10.AutoSize = true;
-            label10.Location = new Point(41, 577);
+            label10.Location = new Point(41, 549);
             label10.Name = "label10";
             label10.Size = new Size(63, 20);
             label10.TabIndex = 31;
@@ -226,11 +271,11 @@
             // childFormPanel
             // 
             childFormPanel.BorderStyle = BorderStyle.FixedSingle;
-            childFormPanel.Controls.Add(dateTimePicker1);
+            childFormPanel.Controls.Add(button5);
+            childFormPanel.Controls.Add(button4);
             childFormPanel.Controls.Add(button3);
-            childFormPanel.Controls.Add(cbboxCapbac);
-            childFormPanel.Controls.Add(nbSoluong);
-            childFormPanel.Controls.Add(dtpkNgayTD);
+            childFormPanel.Controls.Add(label11);
+            childFormPanel.Controls.Add(textBox10);
             childFormPanel.Controls.Add(pictureBox1);
             childFormPanel.Controls.Add(label3);
             childFormPanel.Controls.Add(button2);
@@ -243,74 +288,80 @@
             childFormPanel.Controls.Add(label6);
             childFormPanel.Controls.Add(label9);
             childFormPanel.Controls.Add(textBox5);
+            childFormPanel.Controls.Add(textBox7);
             childFormPanel.Controls.Add(label4);
             childFormPanel.Controls.Add(label8);
+            childFormPanel.Controls.Add(textBox2);
+            childFormPanel.Controls.Add(textBox6);
             childFormPanel.Controls.Add(label5);
             childFormPanel.Controls.Add(label1);
             childFormPanel.Controls.Add(textBox3);
+            childFormPanel.Controls.Add(textBox4);
             childFormPanel.Controls.Add(label7);
             childFormPanel.Location = new Point(0, 0);
             childFormPanel.Name = "childFormPanel";
             childFormPanel.Size = new Size(1077, 721);
             childFormPanel.TabIndex = 33;
             // 
-            // dateTimePicker1
+            // button5
             // 
-            dateTimePicker1.CalendarFont = new Font("Segoe UI", 11F);
-            dateTimePicker1.Font = new Font("Segoe UI", 10F);
-            dateTimePicker1.Location = new Point(884, 169);
-            dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(180, 30);
-            dateTimePicker1.TabIndex = 38;
+            button5.Location = new Point(581, 589);
+            button5.Name = "button5";
+            button5.Size = new Size(148, 40);
+            button5.TabIndex = 39;
+            button5.Text = "Hủy";
+            button5.Visible = false;
+            button5.Click += button5_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(581, 648);
+            button4.Name = "button4";
+            button4.Size = new Size(148, 40);
+            button4.TabIndex = 38;
+            button4.Text = "Lưu";
+            button4.Visible = false;
+            button4.Click += button4_Click;
             // 
             // button3
             // 
-            button3.Location = new Point(85, 344);
+            button3.Location = new Point(172, 648);
             button3.Name = "button3";
-            button3.Size = new Size(128, 40);
+            button3.Size = new Size(270, 40);
             button3.TabIndex = 37;
-            button3.Text = "Up ảnh";
+            button3.Text = "Phản hồi từ doanh nghiệp";
             button3.Click += button3_Click;
             // 
-            // cbboxCapbac
+            // label11
             // 
-            cbboxCapbac.Font = new Font("Segoe UI", 10F);
-            cbboxCapbac.FormattingEnabled = true;
-            cbboxCapbac.Location = new Point(509, 305);
-            cbboxCapbac.Name = "cbboxCapbac";
-            cbboxCapbac.Size = new Size(255, 31);
-            cbboxCapbac.TabIndex = 36;
+            label11.AutoSize = true;
+            label11.Location = new Point(320, 330);
+            label11.Name = "label11";
+            label11.Size = new Size(72, 20);
+            label11.TabIndex = 35;
+            label11.Text = "Kỹ Năng: ";
             // 
-            // nbSoluong
+            // textBox10
             // 
-            nbSoluong.Font = new Font("Segoe UI", 10F);
-            nbSoluong.Location = new Point(509, 262);
-            nbSoluong.Name = "nbSoluong";
-            nbSoluong.Size = new Size(97, 30);
-            nbSoluong.TabIndex = 35;
+            textBox10.BackColor = SystemColors.Window;
+            textBox10.Location = new Point(438, 330);
+            textBox10.Name = "textBox10";
+            textBox10.ReadOnly = true;
+            textBox10.Size = new Size(556, 27);
+            textBox10.TabIndex = 36;
             // 
-            // dtpkNgayTD
-            // 
-            dtpkNgayTD.CalendarFont = new Font("Segoe UI", 11F);
-            dtpkNgayTD.Font = new Font("Segoe UI", 10F);
-            dtpkNgayTD.Location = new Point(509, 169);
-            dtpkNgayTD.Name = "dtpkNgayTD";
-            dtpkNgayTD.Size = new Size(180, 30);
-            dtpkNgayTD.TabIndex = 33;
-            // 
-            // NV_DangBai
+            // NV_TraCuuHD_XemBaiDang
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1077, 721);
             Controls.Add(childFormPanel);
             FormBorderStyle = FormBorderStyle.None;
-            Name = "NV_DangBai";
+            Name = "NV_TraCuuHD_XemBaiDang";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             childFormPanel.ResumeLayout(false);
             childFormPanel.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)nbSoluong).EndInit();
             ResumeLayout(false);
         }
 
@@ -323,21 +374,25 @@
         private TextBox textBox1;
         private TextBox textBox5;
         private Label label6;
+        private TextBox textBox2;
         private Label label4;
         private TextBox textBox3;
         private Label label5;
+        private TextBox textBox4;
         private Label label7;
+        private TextBox textBox6;
         private Label label1;
+        private TextBox textBox7;
         private Label label8;
         private TextBox textBox8;
         private Label label9;
         private TextBox textBox9;
         private Label label10;
         private Panel childFormPanel;
-        private NumericUpDown nbSoluong;
-        private DateTimePicker dtpkNgayTD;
-        private ComboBox cbboxCapbac;
+        private Label label11;
+        private TextBox textBox10;
         private Button button3;
-        private DateTimePicker dateTimePicker1;
+        private Button button4;
+        private Button button5;
     }
 }
