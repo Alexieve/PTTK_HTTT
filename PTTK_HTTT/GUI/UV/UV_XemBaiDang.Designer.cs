6c28af403f323413e0febcb6dc2774cba594f372
TruongVinhPhuc <truongvinhphucpro7@gmail.com>
2024-05-11 19:31:27 +0700

    fix ava BaiDangTuyenDung

--- PTTK_HTTT/GUI/UV/UV_XemBaiDang.Designer.cs
+++ PTTK_HTTT/GUI/UV/UV_XemBaiDang.Designer.cs
@@ -91,7 +91,7 @@
             // pictureBox1
             // 
             pictureBox1.BackColor = Color.Transparent;
-            pictureBox1.Image = Properties.Resources.Trollface_non_free;
+            pictureBox1.Image = Properties.Resources.tải_xuống;
             pictureBox1.Location = new Point(41, 120);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(245, 218);
